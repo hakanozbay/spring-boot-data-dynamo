092ffb6a2c2791aafcdf7faf576a88097eb4d033
Hakan Ozbay <hakan@grabyo.com>
2021-01-13 11:22:20 +0000

    - add additional rest endpoint to create user

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.User;
@@ -12,10 +13,14 @@ import com.example.repository.UserRepository;
 @RestController
 public class UserController {
 	
-	//@PostMapping("/createUsers")
-	
 	@Autowired
 	UserRepository repository;
+	
+	@GetMapping("/createUser")
+	public void createUser()
+	{
+		repository.save(new User ("a","b"));
+	}
 	
 	@GetMapping("/count")
 	public long countUsers()
